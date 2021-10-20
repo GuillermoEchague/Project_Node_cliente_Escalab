c6ccdd99d555a94d010641900426e6fd0fabed92
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2021-10-20 00:11:18 -0300

    save please!

--- src/components/Admin/MenuTop/MenuTop.js
+++ src/components/Admin/MenuTop/MenuTop.js
@@ -1,26 +1,34 @@
 import React from "react";
 import {Button, Icon} from 'antd';
 import ImgLogo from '../../../assets/img/png/imgprueba.png';
+import {logout} from '../../../api/auth';
+
 import './MenuTop.scss';
 
 export default function MenuTop(props){
     const {menuCollapsed, setMenuCollapsed} = props;
-    return (
+    const logoutUser = () => {
+        logout();
+        window.location.reload();
+      };
+
+      return (
         <div className="menu-top">
-            <div className="menu-top__left">
-                <img className="menu-top__left-logo"
+          <div className="menu-top__left">
+            <img
+              className="menu-top__left-logo"
                 src={ImgLogo}
                 alt="foto-menu"
                 />
-                <Button type="link" onClick={()=>setMenuCollapsed(!menuCollapsed)}>
-                    <Icon type={menuCollapsed ? "close":"menu-fold"} />
-                </Button>
-            </div>
-            <div className="menu-top__right">
-                <Button type="link" onClick={()=> console.log("Desconexion")}>
-                    <Icon type="poweroff"/>
-                </Button>
-            </div>
-        </div>
-    );
+        <Button type="link" onClick={() => setMenuCollapsed(!menuCollapsed)}>
+          <Icon type={menuCollapsed ? "menu-unfold" : "menu-fold"} />
+        </Button>
+      </div>
+      <div className="menu-top__right">
+        <Button type="link" onClick={logoutUser}>
+          <Icon type="poweroff" />
+        </Button>
+      </div>
+    </div>
+  );
 }
